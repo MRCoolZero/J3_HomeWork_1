5cdc423fc23d4d8cc07807bca2569d9b7cb03793
MRZeroCool <kaka.kaka1234.kaka1234@mail.ru>
2020-05-14 07:50:39 +0300

    Java3_HomeWork_1

--- src/ru/rickSanchez/company/Apple.java
+++ src/ru/rickSanchez/company/Apple.java
@@ -1,5 +1,8 @@
 package ru.rickSanchez.company;
 
 public class Apple extends Fruit {
-
+    //вес яблока - 1.0f
+    public Apple() {
+        super(1.0f);
+    }
 }
--- src/ru/rickSanchez/company/Orange.java
+++ src/ru/rickSanchez/company/Orange.java
@@ -2,4 +2,8 @@ package ru.rickSanchez.company;
 
 public class Orange extends Fruit {
 
+    //апельсина - 1.5f
+    public Orange() {
+        super(1.5f);
+    }
 }
